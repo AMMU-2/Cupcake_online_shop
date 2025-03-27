a3e94dc1d08a0cea0db42a4ac139374a815a7a61
AMMU-2 <121598889+AMMU-2@users.noreply.github.com>
2025-03-27 15:08:04 +0530

    Update cupcake.js

--- frontend/src/components/cupcake.js
+++ frontend/src/components/cupcake.js
@@ -1,20 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import '../css/cupcakeHome.css';
- 
-const Cupcake = ({ image, cakeName, description, price, onView, onAddToCart  }) => {
-  
+
+const Cupcake = ({ image, cakeName, description, price, onView, onAddToCart }) => {
   return (
-  <div className="cupcakeDiv col-3 col-t-6 col-m-6">
-    <img src={`http://localhost:5000/${image}`} alt={cakeName} />
-    <h6><b>{cakeName}</b></h6>
-    <div className="cupcakeText">
-      <p>{description}</p>
+    <div className="cupcakeDiv col-3 col-t-6 col-m-6">
+   
+      <img src={`http://localhost:5000/${image}`} alt={cakeName} />
+      <h6><b>{cakeName}</b></h6>
+     
+      <div className="cupcakeText">
+        <p>{description}</p>
+      </div>
+      
+      <h6><b>Rs. {price}</b></h6>
+ 
+      <button className="link" onClick={onView}>View</button> 
+      <button className="link" onClick={onAddToCart}>Add to cart</button>
     </div>
-    <h6><b>Rs. {price}</b></h6>
-    <button className="link" onClick={onView}>View</button> {/* ✅ Updated */}
-    <button className="link" onClick={onAddToCart}>Add to cart</button>
-  </div>
   );
 };
- 
+
 export default Cupcake;
