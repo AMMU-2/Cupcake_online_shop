3bae0a990e5af34b2ad90842cddba7ae18ca6b15
SUDESHNA DAS <134364304+susd12@users.noreply.github.com>
2025-03-27 14:49:47 +0530

    Update shoppingcart.model.js

--- backend/models/shoppingcart.model.js
+++ backend/models/shoppingcart.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//defing the cartSchema for each userid having cartid
 const cartSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true },
     createdAt: {type:Date, default:Date.now}
