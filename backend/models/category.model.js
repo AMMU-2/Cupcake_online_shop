a7d9485cd7940157378bb2c2a886ca81a17cce6d
SUDESHNA DAS <134364304+susd12@users.noreply.github.com>
2025-03-27 14:45:09 +0530

    Update category.model.js

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
- 
+ //defing the categorySchema
 const categorySchema = new mongoose.Schema({
   // cakeId: { type: String, required: true, unique: true },
   categoryName:{type:String, required:true}
