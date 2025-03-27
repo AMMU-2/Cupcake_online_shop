77569e968ce6a5734125955f0a44457017aeb108
AMMU-2 <121598889+AMMU-2@users.noreply.github.com>
2025-03-27 14:56:26 +0530

    Update PopupModel.js

--- frontend/src/components/PopupModel.js
+++ frontend/src/components/PopupModel.js
@@ -1,17 +1,18 @@
-
 import React from "react";
 import "../css/popupmodel.css"; 
 
+// Popup modal component for login prompt
 const PopupModal = ({ show, onClose, onLogin }) => {
   if (!show) return null; 
 
   return (
-    <div className="popup-overlay">
-      <div className="popup-modal">
-        <p>You need to log in to add cakes to the cart.</p>
-        <div className="popup-buttons">
-          <button onClick={onLogin}>Go to Login</button>
-          <button onClick={onClose}>Close</button>
+    <div className="popup-overlay"> 
+      <div className="popup-modal"> 
+        <p>You need to log in to add cakes to the cart.</p> 
+        
+        <div className="popup-buttons"> {/* Button container */}
+          <button onClick={onLogin}>Go to Login</button> {/* Redirects to login page */}
+          <button onClick={onClose}>Close</button> {/* Closes the popup */}
         </div>
       </div>
     </div>
